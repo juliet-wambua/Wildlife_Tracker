3c0273fedb1f66e52a28f320e5b0edee70b156c5
juliet-wambua <julienea289@gmail.com>
2019-08-05 12:41:08 +0300

    tests

--- src/test/java/Models/EndangeredTest.java
+++ src/test/java/Models/EndangeredTest.java
@@ -2,10 +2,14 @@ package Models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class EndangeredTest {
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
 
     @Before
     public void setUp() throws Exception {
@@ -14,4 +18,64 @@ public class EndangeredTest {
     @After
     public void tearDown() throws Exception {
     }
+
+    @Test
+    public void endangeredAnimal_InstantiatesCorrectly_true(){
+        Endangered myEndangeredAnimal = new Endangered("panther","young","weak","");
+        assertEquals(true, myEndangeredAnimal instanceof Endangered);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void animal_InstantiatesWithName_panther(){
+        Endangered myEndangeredAnimal = new Endangered("","","","");
+        assertEquals("panther", myEndangeredAnimal.getName());
+    }
+
+    // test to see if animal instantiates with an age and if an error is thrown
+    @Test (expected = IllegalArgumentException.class)
+    public void EndangeredAnimal_InstantiatesWithAge_mature(){
+        Endangered myEndangeredAnimal = new Endangered("","","","");
+    }
+
+    // test to see if animal instantiates with the health status and if an error is thrown
+    @Test (expected = IllegalArgumentException.class)
+    public void EndangeredAnimal_InstantiatesWithHealth_Weak(){
+        Endangered myEndangeredAnimal = new Endangered("","","","");
+    }
+    //test to check if info is saved into database
+    @Test
+    public void EndangeredAnimal_IsSavedInDatabase(){
+        Endangered myEndangeredAnimal = new Endangered("panther", "adult", "healthy","endangered");
+        myEndangeredAnimal.save();
+        assertTrue(myEndangeredAnimal.all().get(0).equals(myEndangeredAnimal));
+    }
+    //Test to find Animal with the Same Id
+    @Test
+    public void find_WillReturnEndangeredAnimalWithTheSame_SecondAnimal(){
+        Endangered firstEndangeredAnimal = new Endangered("panther", "adult", "healthy","endangered");
+        firstEndangeredAnimal.save();
+        Endangered SecondEndangeredAnimal = new Endangered("panther", "adult", "healthy","endangered");
+        SecondEndangeredAnimal.save();
+        assertEquals(Endangered.find(SecondEndangeredAnimal.getId()), SecondEndangeredAnimal);
+    }
+    //Animal is assigined an Id
+    @Test
+    public void EndangeredAnimal_AnimalIsAssignedAnID_getId(){
+        Endangered myEndangeredAnimal = new Endangered("panther", "adult", "healthy","endangered");
+        myEndangeredAnimal.save();
+        Endangered testEndangeredAnimals = Endangered.all().get(0);
+        assertEquals(myEndangeredAnimal.getId(), testEndangeredAnimals.getId());
+    }
+    @Test
+    public void EndangeredAnimal_AllInstancesOfAnimalAreReturned_True(){
+        Endangered myEndangeredAnimal1 = new Endangered("panther", "adult", "healthy","endangered");
+        myEndangeredAnimal1.save();
+        Endangered myEndangeredAnimal2 = new Endangered("Black Rhino", "adult", "healthy","endangered");
+        myEndangeredAnimal2.save();
+        assertTrue(Endangered.all().get(0).equals(myEndangeredAnimal1));
+        assertTrue(Endangered.all().get(1).equals(myEndangeredAnimal2));
+    }
+
+
+
+
 }
--- src/test/java/Models/SightingsTest.java
+++ src/test/java/Models/SightingsTest.java
@@ -2,10 +2,14 @@ package Models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class SightingsTest {
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
 
     @Before
     public void setUp() throws Exception {
@@ -14,4 +18,41 @@ public class SightingsTest {
     @After
     public void tearDown() throws Exception {
     }
+
+    @Test
+    public void Sighting_InstantiatesCorrectly_True(){
+        Sightings newsighting = new Sightings("Roger","Zone 1",1);
+        assertTrue( newsighting instanceof Sightings);
+    }
+    @Test
+    public void Sighting_IsSavedOnDataBase_True(){
+        Sightings newSighting = new Sightings("Roger","Zone 2",1);
+        newSighting.save();
+        assertTrue(Sightings.all().get(0).equals(newSighting));
+    }
+    @Test
+    public void sighting_EachSightingIsAssignedAnId_getid(){
+        Sightings newSighting = new Sightings("Roger","Zone 2",1);
+        newSighting.save();
+        Sightings testSighting = Sightings.all().get(0);
+        assertEquals(newSighting.getId(), testSighting.getId());
+    }
+    @Test
+    public void find_WillReturnSightingWithTheSameID_SecondSighting(){
+        Sightings firstSighting = new Sightings("Roger","Zone 2",1);
+        firstSighting.save();
+        Sightings SecondSighting = new Sightings("Chris","Zone 5",3);
+        SecondSighting.save();
+        assertEquals(SecondSighting, Sightings.find(SecondSighting.getId()));
+    }
+    @Test
+    public void Sightings_CanBeDeletedFromDataBase_true() {
+        Sightings newSighting = new Sightings("Brenden","Zone 5",1);
+        newSighting.save();
+        newSighting.delete();
+        assertEquals(0, Sightings.all().size());
+    }
+
+
+
 }
