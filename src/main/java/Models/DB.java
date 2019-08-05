485ea3ddd28f15ef449451bb8a596c35790cf555
juliet-wambua <julienea289@gmail.com>
2019-08-05 11:52:08 +0300

    add content to the class of endangred animals

--- src/main/java/Models/DB.java
+++ src/main/java/Models/DB.java
@@ -1,4 +1,8 @@
 package Models;
 
+import org.sql2o.Sql2o;
+
 public class DB {
+    //public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "postgres", "Bus-242-001/2014");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-174-129-29-101.compute-1.amazonaws.com:5432/demlj5phrse0s7", "nthvfprikqqmgr", "0adf07b46a043e20e3f3ac970eba598f3642c3d27a35e7c89c9ea8c8113ab96b");
 }
--- src/main/java/Models/Endangered_Animals.java
+++ src/main/java/Models/Endangered_Animals.java
@@ -1,5 +1,56 @@
 package Models;
 
-public class Endangered_Animals {
+import org.sql2o.Connection;
+
+import java.util.List;
+
+public class Endangered extends  AnimalsAbstract {
+    private static final String ANIMAL_TYPE = "endangered";
+    public Endangered(String name, String age, String health, String type) {
+        if (name.equals("")) {
+            throw new IllegalArgumentException("Please enter a name of the animal you want to add");
+        }
+        if (age.equals("")) {
+            throw new IllegalArgumentException("Please enter the age of the animal");
+        }
+        if (health.equals("")) {
+            throw new IllegalArgumentException("Please enter the health status of the animal");
+        }
+        this.name = name;
+        this.age = age;
+        this.health = health;
+        this.type = ANIMAL_TYPE;
+    }
+    @Override
+    public boolean equals(Object otherAnimal){
+        if(!(otherAnimal instanceof Object)){
+            return false;
+        }
+        AnimalsAbstract myAnimal = (AnimalsAbstract) otherAnimal;
+        return this.getName().equals(myAnimal.getName())&&
+                this.getType().equals(myAnimal.getType())&&
+                this.getId()==myAnimal.getId() ;
+
+    }
+    @Override
+    public void save(){
+        try(Connection con = DB.sql2o.open()){
+            String sql = "INSERT INTO animal (name, age, health, type) VALUES (:name, :age, :health, :type);";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .addParameter("age", this.age)
+                    .addParameter("health", this.health)
+                    .addParameter("type", this.type)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+    public static List<Endangered> all(){
+        String sql = "SELECT * FROM animal WHERE type='endangered'";
+        try(org.sql2o.Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql).executeAndFetch(Endangered.class);
+        }
+    }
 
 }
+
