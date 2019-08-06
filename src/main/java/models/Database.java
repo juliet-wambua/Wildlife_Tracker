45b417487b47fec24e16c445b0462fa70dc591f9
juliet-wambua <julienea289@gmail.com>
2019-08-06 12:29:13 +0300

    tables in psql

--- src/main/java/models/Database.java
+++ src/main/java/models/Database.java
@@ -3,5 +3,5 @@ package models;
 import org.sql2o.Sql2o;
 
 public class Database {
-    public static Sql2o sql2otest = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "moringaschool", "1543");
+    public static Sql2o sql2otest = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "moringaschool", "1543");
 }
