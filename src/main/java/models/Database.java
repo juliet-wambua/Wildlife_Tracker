4ab6dfd773f042d868f15a3f811a0c4ff80f1318
juliet-wambua <julienea289@gmail.com>
2019-08-06 17:45:04 +0300

    this commit will add pom.xml file

--- src/main/java/models/Database.java
+++ src/main/java/models/Database.java
@@ -3,5 +3,5 @@ package models;
 import org.sql2o.Sql2o;
 
 public class Database {
-    public static Sql2o sql2otest = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "moringaschool", "1543");
+    public static Sql2o sql2otest = new Sql2o("jdbc:postgresql://ec2-107-20-243-220.compute-1.amazonaws.com:5432/d8vo75qvp1d84l", "foisumzmptunls", "59602abbc96bdb9a673321ec69bbbd2c14c3ea0f992c568fb7500db4097b7856");
 }
